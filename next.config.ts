dddcf12a1d6b82b4e93e546000353dee8b97c7e0
giangdaiduong <giangdaiduong@gmail.com>
2025-05-29 10:11:31 +0700

    refactor: streamline redirects configuration and update image remote patterns for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -2,39 +2,32 @@ import type { NextConfig } from 'next';
 import linkTo from '@/utils/linkTo';
 
 const nextConfig: NextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: linkTo.admin.base,
-        destination: linkTo.admin.dashboard,
-        permanent: true,
-        locale: false,
-      },
-      {
-        source: linkTo.investor.base,
-        destination: linkTo.investor.listIdea,
-        permanent: true,
-        locale: false,
-      },
-    ];
-  },
+  redirects: async () => [
+    {
+      source: linkTo.admin.base,
+      destination: linkTo.admin.dashboard,
+      permanent: true,
+      locale: false,
+    },
+    {
+      source: linkTo.investor.base,
+      destination: linkTo.investor.listIdea,
+      permanent: true,
+      locale: false,
+    },
+  ],
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: '**',
+        pathname: '/**',
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images2.thanhnien.vn',
+        protocol: 'http',
+        hostname: '**',
+        pathname: '/**',
       },
     ],
   },
